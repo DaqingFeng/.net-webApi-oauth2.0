21eb76db4502cec2609945bb57d92a95025a1d16
fengdaqing <fengdaqing@yiguo.com>
2017-08-03 10:17:12 +0800

    OAuth2.0 Sample uploading.

---
+++ WebAppOauth/App_Start/IdentityConfig.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using WebAppOauth.Models;
+
+namespace WebAppOauth
+{
+    public class EmailService : IIdentityMessageService
+    {
+        public Task SendAsync(IdentityMessage message)
+        {
+            // Plug in your email service here to send an email.
+            return Task.FromResult(0);
+        }
+    }
+
+    public class SmsService : IIdentityMessageService
+    {
+        public Task SendAsync(IdentityMessage message)
+        {
+            // Plug in your SMS service here to send a text message.
+            return Task.FromResult(0);
+        }
+    }
+
+
+    public class UserStoreManager : IUserStore<ApplicationUser>
+    {
+
+        public static UserStoreManager userstore = null;
+        private static object locks = new object();
+        public static UserStoreManager Instance()
+        {
+            lock (locks)
+            {
+                if (userstore == null)
+                {
+                    userstore = new UserStoreManager();
+                }
+                return userstore;
+            }
+        }
+
+        public Task CreateAsync(ApplicationUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(ApplicationUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+
+        }
+
+        public Task<ApplicationUser> FindByIdAsync(string userId)
+        {
+            return Task.FromResult(new ApplicationUser() { UserName = "fdaqing" });
+        }
+
+        public Task<ApplicationUser> FindByNameAsync(string userName)
+        {
+            return Task.Run(() =>
+            {
+                return new ApplicationUser() { UserName = "daiqng" };
+            });
+        }
+
+
+        public Task UpdateAsync(ApplicationUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+    }
+
+    // Configure the application user manager used in this application. UserManager is defined in ASP.NET Identity and is used by the application.
+    public class ApplicationUserManager : UserManager<ApplicationUser>
+    {
+        public ApplicationUserManager(IUserStore<ApplicationUser> store)
+            : base(store)
+        {
+        }
+
+        public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options, IOwinContext context)
+        {
+            var manager = new ApplicationUserManager(UserStoreManager.Instance());
+            // Configure validation logic for usernames
+            manager.UserValidator = new UserValidator<ApplicationUser>(manager)
+            {
+                AllowOnlyAlphanumericUserNames = false,
+                RequireUniqueEmail = true
+            };
+
+            // Configure validation logic for passwords
+            manager.PasswordValidator = new PasswordValidator
+            {
+                RequiredLength = 6,
+                RequireNonLetterOrDigit = true,
+                RequireDigit = true,
+                RequireLowercase = true,
+                RequireUppercase = true,
+            };
+
+            // Configure user lockout defaults
+            manager.UserLockoutEnabledByDefault = true;
+            manager.DefaultAccountLockoutTimeSpan = TimeSpan.FromMinutes(5);
+            manager.MaxFailedAccessAttemptsBeforeLockout = 5;
+
+            // Register two factor authentication providers. This application uses Phone and Emails as a step of receiving a code for verifying the user
+            // You can write your own provider and plug it in here.
+            manager.RegisterTwoFactorProvider("Phone Code", new PhoneNumberTokenProvider<ApplicationUser>
+            {
+                MessageFormat = "Your security code is {0}"
+            });
+            manager.RegisterTwoFactorProvider("Email Code", new EmailTokenProvider<ApplicationUser>
+            {
+                Subject = "Security Code",
+                BodyFormat = "Your security code is {0}"
+            });
+            manager.EmailService = new EmailService();
+            manager.SmsService = new SmsService();
+            var dataProtectionProvider = options.DataProtectionProvider;
+            if (dataProtectionProvider != null)
+            {
+                manager.UserTokenProvider =
+                    new DataProtectorTokenProvider<ApplicationUser>(dataProtectionProvider.Create("ASP.NET Identity"));
+            }
+            return manager;
+        }
+
+        public override Task<ApplicationUser> FindAsync(string userName, string password)
+        {
+            return Task.Run(() =>
+            {
+                ApplicationUser user = new Models.ApplicationUser();
+                user.UserName = "Fengdaqing";
+                user.Email = "fengdaqing@hotmail.com";
+                return user;
+            });
+        }
+    }
+
+    // Configure the application sign-in manager which is used in this application.
+    public class ApplicationSignInManager : SignInManager<ApplicationUser, string>
+    {
+        public ApplicationSignInManager(ApplicationUserManager userManager, IAuthenticationManager authenticationManager)
+            : base(userManager, authenticationManager)
+        {
+        }
+
+        public override Task<ClaimsIdentity> CreateUserIdentityAsync(ApplicationUser user)
+        {
+            return user.GenerateUserIdentityAsync((ApplicationUserManager)UserManager);
+        }
+
+        public static ApplicationSignInManager Create(IdentityFactoryOptions<ApplicationSignInManager> options, IOwinContext context)
+        {
+            return new ApplicationSignInManager(context.GetUserManager<ApplicationUserManager>(), context.Authentication);
+        }
+
+        public override Task<SignInStatus> PasswordSignInAsync(string userName, string password, bool isPersistent, bool shouldLockout)
+        {
+            return Task.Run(() =>
+            {
+                ApplicationUser user = new Models.ApplicationUser();
+                user.UserName = "Fengdaqing";
+                user.Email = "fengdaqing@hotmail.com";
+                base.SignInAsync(user, true, true);
+                return SignInStatus.Success;
+            });
+        }
+    }
+}
---
+++ WebAppOauth/Controllers/CommonController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace WebAppOauth.Controllers
+{
+    [Authorize]
+    public class CommonController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return HttpContext.Current.User.Identity.Name;
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
